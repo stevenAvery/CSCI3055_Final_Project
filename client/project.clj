e356195a493e1a43fa96efe5d3fbaa531bf69a07
savery <steven.avery@uoit.net>
2016-04-09 21:48:29 -0400

    Created lein project for server

--- client/project.clj
+++ client/project.clj
@@ -1,5 +1,5 @@
 (defproject none "0.1.0-SNAPSHOT"
-	:description "CSCI 3055U Final Project"
+	:description "CSCI 3055U Final Project Client"
 	:url "https://github.com/stevenAvery/CSCI3055_Final_Project"
 
 	:dependencies [[org.clojure/clojure "1.7.0"]
---
+++ server/project.clj
@@ -0,0 +1,8 @@
+(defproject none "0.1.0-SNAPSHOT"
+	:description "CSCI 3055U Final Project Server"
+	:url "https://github.com/stevenAvery/CSCI3055_Final_Project"
+
+	:dependencies [[org.clojure/clojure "1.7.0"]]
+
+
+	:source-paths ["src/clj"])
